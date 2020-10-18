1741122e590a4e396611cf9fb67dba876290a414
Gennadiy-Alexandrovich <Dom2933@yandex.ru>
2020-10-18 23:01:34 +0500

    add

--- script/getData.js
+++ script/getData.js
@@ -2,21 +2,35 @@
 
 export const getData = {
   url: 'database/dataBase.json', //создаем  url до нашей базы
-  get(process) {//метод get мы передаем колбек ф-ю process (колбек ф-я будет запускать обработку)
+  get(process) {//В метод get мы передаем колбек ф-ю process (колбек ф-я будет запускать обработку)
     fetch(this.url)//глобальный метод fetch(), который позволяет легко и логично получать ресурсы по сети асинхронно, АПи запрос на сервер по URL
       //асинхронно глобального метода fetch(), который возвращает Promise с объектом типа Response который мы обрабатываем then
       //_promise
-      //.then(result => {...})обработка 
-      //.catch(error => {...})ошибка 
+      //.then(result => {...})обработка Promise
+      //.catch(error => {...})обработка ошибки
       //.finally(() => {...});
-      .then((response) => response.json())//В переменной response мы получаем поток данный с сервера (ReadableStream) и преобразуем эти данные в json  формат then -передает promise дальше по цепочке
+      .then((response) => response.json())//В then мы передаем ф-ю (response) переменной response мы получаем поток данный с сервера (ReadableStream) и преобразуем эти данные в json  формат then -передает promise дальше по цепочке
       //.then((data) =>  console.log(data))//В переменной data мы получаем данные в json формате
       //.then((data) =>  1)
-      .then((data) => console.log(data)
-      
-    // .then(process);
-   
-    }
-};
+      // .then((data) => console.log(data))//В переменной data мы получаем данные в json формате
+      // .then((data) => 1)//Здесь мы присвоили 1 и в консоле выходит 1
+      // .then((data) => console.log(data))
 
-getData.get();
+      .then(process);//Здесь запускается ф-я process с теми данными которые мы передадим
+
+  },
+  //Разные методы для получения списка которые будем по разному обрабатывать
+
+  wishList(list, callback) {
+    this.get((data) => {
+      const result = data.filter((item) => list.includes(item.id));
+      callback(result);
+    })
+  }
+
+
+};
+// getData.get();
+
+
+
--- script/loadData.js
+++ script/loadData.js
@@ -2,47 +2,60 @@
 
 import { getData } from './getData.js';
 
-const wichList = ['idd010', 'idd030', 'idd100', 'idd50'];//Лист желаний
-  
+const wishList = ['idd010', 'idd030', 'idd100', 'idd050'];//Лист желаний
+
 const cardList = [//Спиок корзины массив с обьектом(кроме id: 'idd010' нужно еще колличество: count: 4) 
-    {
-      id: 'idd010',
-      count: 4
-    },
-    {
-      id: 'idd014',
-      count: 6
-    },
-    {
-      id: 'idd077',
-      count: 2
-    }
+  {
+    id: 'idd010',
+    count: 4
+  },
+  {
+    id: 'idd014',
+    count: 6
+  },
+  {
+    id: 'idd077',
+    count: 2
+  }
 
 
 ];
-  
+
 export const loadData = () => {
-    //Есть ОБЬЕКТ location ( если набрать в консоле location) в этом обьекте есть search и hash иpathname и тд
-    //B hash: 'idd010' (айдишник нашего товара)
-    //B search: '(название нашего товара нашего товара)
-    if(location.search){//обрашаемся к location и проверяем есть ли там search
-      const search = decodeURI(location.search);//Метод decodeURI() декодирует унифицированный идентификатор ресурса (URI) получаем массив search  с данными на русском языке
-      console.log(location.search)
-      console.log(search)
-      const prop = search.split('=')[0].substring(1);//разбей данные  массива search split по Знаку равно нулевой элемент и убери первый символ
-      console.log(prop)
-      const value = search.split('=')[1];//разбей данные массива search  split по Знаку равно первый элемент
-      console.log(value)
-  
+  //Есть ОБЬЕКТ location ( если набрать в консоле location) в этом обьекте есть search и hash иpathname и тд
+  //B hash: 'idd010' (айдишник нашего товара)
+  //B search: '(название нашего товара нашего товара)
+  if (location.search) {//обрашаемся к location и проверяем есть ли там search
+    const search = decodeURI(location.search);//Метод decodeURI() декодирует унифицированный идентификатор ресурса (URI) получаем массив search  с данными на русском языке
+    console.log(location.search)
+    console.log(search)
+    const prop = search.split('=')[0].substring(1);//разбей данные  массива search split по Знаку равно выведи в prop нулевой элемент и убери первый символ
+    console.log(prop)
+    const value = search.split('=')[1];//разбей данные массива search  split по Знаку равно и выведи в value первый элемент 
+    console.log(value)
+    console.log(location);
 
-    }
-    if(location.hash){//обрашаемся к location и проверяем есть ли hash
-    console.log("hash")
 
-    }
-    if(location.pathname.includes('cart')){//обрашаемся к location и проверяем есть ли  в pathname(путь к ресурсу) cart
-    console.log("pathname")
 
+    if (prop === 's') {
+      console.log('value');
+    } else if (prop === 'wishlist') {
+      getData.wishList(wishList, (data) => console.log(data));
+      //импортируем getData из файла getData.js и примеяем ему метод wichList с 
+      //параметром const wichList = ['idd010', 'idd030', 'idd100', 'idd50'];//Лист желаний.  и с колбек ф-ей Сам метод wichList(wichList) прописан в файле getData.js
+      //console.log(wichList);
+    } else {
+      console.log(prop, value);
     }
+  }
+
+  if (location.hash) {//обрашаемся к location и проверяем есть ли hash
+    console.log(location.hash.substring(1));
+
+  }
+  if (location.pathname.includes('cart')) {//обрашаемся к location и проверяем есть ли  в pathname(путь к ресурсу) cart
+    console.log(cardList)
+
+  }
 
 };
